b9b801c5676a1ea1faee33df3f80bc49554d8320
vlad424 <vlad6754687@gmail.com>
2022-04-10 19:21:25 +0700

    samyth start

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div className="NotFoundPage">
+            
+        </div>
+    );
+};
+
+export default NotFoundPage;
