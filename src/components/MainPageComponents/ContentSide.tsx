a6e846ba12c7be58f18af8a75fcb120ac0e1f0d1
vlad424 <vlad6754687@gmail.com>
2022-04-15 14:23:51 +0700

    pull

--- src/components/MainPageComponents/ContentSide.tsx
+++ src/components/MainPageComponents/ContentSide.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import './css/ContentSide.css'
+
 const ContentSide = () => {
     return (
-        <div className='content-side'>
-            
+        <div className='main-content-side'>
+            <div className="main-header">
+                this is empty block
+            </div>
+            <div className="main-content">
+                {/* here component */}
+            </div>
         </div>
     );
 };
