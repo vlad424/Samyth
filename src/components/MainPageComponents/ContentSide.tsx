53c7dfb77ae452c2aac7967d9494ace56c653190
vlad424 <vlad6754687@gmail.com>
2022-04-17 19:23:10 +0700

    work on post elements

--- src/components/MainPageComponents/ContentSide.tsx
+++ src/components/MainPageComponents/ContentSide.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import './css/ContentSide.css'
+import "./css/ContentSide.css";
 
 const ContentSide = () => {
-    return (
-        <div className='main-content-side'>
-            <div className="main-header">
-                this is empty block
-            </div>
-            <div className="main-content">
-                {/* here component */}
-            </div>
-        </div>
-    );
+  const mass = {
+    hello: [
+      { content: "none" },
+      { content: "none", img: "", logo: "" },
+    ],
+  };
+  return (
+    <div className="main-content-side">
+      <div className="main-header">this is empty block</div>
+      <div className="main-content">
+        {mass.hello.map((el) => {
+          if ("img" in el) {
+            return( 
+              <div className="main-post-allin">
+                <div className="post-header">
+                  <div className="post-up">{el.logo}</div>
+                  <div className="post-down">{el.content}</div>
+                </div>
+                <div className="post-body">{el.img}</div>
+              </div>
+            );
+          } else {
+            return( 
+              <div className="main-post-text">
+                {el.content}
+              </div>
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ContentSide;
